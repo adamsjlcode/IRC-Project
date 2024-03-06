7ded0e96c5ffdd1c3992def6022afe3c37b94903
adamsjlcode <adamsjlcode@gmail.com>
2024-03-06 13:48:32 -0500

    Cleaned CLI

--- chatcli.c
+++ chatcli.c
@@ -10,8 +10,9 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024  // Define the maximum size for message buffers
 
+// ANSI escape codes for colored and bold text in the console
 #define ANSI_STYLE_BOLD   "\e[1m"
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -20,16 +21,14 @@
 
 
 
-int sockfd;        // Socket file descriptor
-char username[32]; // Username for login
+int sockfd;        // Socket file descriptor for the client's connection
+char username[32]; // Array to store the username for this client
 
 // A ANSI color code to allow a user to define the userid color in thier messages
 // The default color is Green
 // The value must be checked to ensure the user is not entering another ANSI escape sequence or colors that are not allowed.
 char colorid[] = "32";
 
-// char realname[32]; // Real name of the user
-// char password[32]; // Password for login
 
 // Function to overwrite the current line in stdout
 void str_overwrite_stdout() {
@@ -55,23 +54,22 @@ void catch_ctrl_c_and_exit(int sig) {
   exit(EXIT_SUCCESS);
 }
 
-//Startup Usage Menu
+// Function to display usage instructions for starting the client
 void print_usage(char *program_name) {
     fprintf(stderr,
         // "Usage: %s -u username -r realname -p password -a address:port\n"
-        "Usage: %s -u username -c ansicode -a address:port\n"
-        "\t-u  Set the username for the login.\n"
-        // "  -r  Set the real name of the user\n"
-        // "  -p  Set the password for the login\n"
-        "\t-a  Set the IP address and port of the server in the format address:port.\n"
+        "Usage: %s -u username -a address:port\n"
+        "\t-u  Set the username for the login\n"
+        "\t-a  Set the IP address and port of the server in the format address:port\n",
         "\t-c  Set the color of your user id. This is visible on other users clients. ASNI colors between 32 and 36 (inclusive) are allowed.\n",
+
         program_name);
 }
 
 // Thread function to handle sending messages
 void *send_msg_handler(void *arg) {
-    char message[BUFFER_SIZE] = {};
-    char buffer[BUFFER_SIZE + 32] = {};
+    char message[BUFFER_SIZE] = {};  // Buffer to store the message to be sent
+    char buffer[BUFFER_SIZE + 32] = {};  // Buffer to store the formatted message
 
     while (1) {
         str_overwrite_stdout();
@@ -91,91 +89,79 @@ void *send_msg_handler(void *arg) {
         bzero(buffer, BUFFER_SIZE + 32);
     }
 
-    catch_ctrl_c_and_exit(2);
+    catch_ctrl_c_and_exit(2);  // Exit if the loop is broken
     return NULL;
 }
 
 // Timestamp function
 void getTimeStamp(char *timestamp) {
-	time_t rawtime;
-	struct tm *timeinfo;
+    time_t rawtime;
+    struct tm *timeinfo;
 
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
 
-	strftime(timestamp, 20, "%Y-%m-%d %H:%M:%S", timeinfo);
+    strftime(timestamp, 20, "%Y-%m-%d %H:%M:%S", timeinfo);
 }
 
 // Thread function to handle receiving messages
 void *recv_msg_handler(void *arg) {
-  char message[BUFFER_SIZE] = {};
-  while (1) {
-    memset(message, 0, BUFFER_SIZE);
-    int receive = recv(sockfd, message, BUFFER_SIZE, 0);
-    char timestamp[20];
-    
-    if (receive > 0) {
-      message[receive] = '\0';  // Null-terminate the message
-      getTimeStamp(timestamp);  // Get the current timestamp
-      
-      // Check if the message is a server shutdown notice
-      if (strcmp(message, "Server is shutting down.\n") == 0) {
-        printf("%s - Server is shutting down. Exiting...\n", timestamp);
-        exit(EXIT_SUCCESS);  // Exit client program
-      }
-      
-      printf("%s - %s", timestamp, message);  // Print the timestamp and message
-      str_overwrite_stdout();  // Overwrite the stdout
-    } else if (receive == 0) {
-      printf("Server connection closed. Exiting...\n");
-      exit(EXIT_SUCCESS);  // Exit client program
-    } else {
-      perror("recv failed");  // Print the receive error
-      exit(EXIT_FAILURE);  // Exit client program due to error
+    char message[BUFFER_SIZE] = {};  // Buffer for incoming messages
+
+    while (1) {
+        memset(message, 0, BUFFER_SIZE);
+        int receive = recv(sockfd, message, BUFFER_SIZE, 0);
+        char timestamp[20];
+        
+        if (receive > 0) {
+            message[receive] = '\0';  // Null-terminate the message
+            getTimeStamp(timestamp);  // Get the current timestamp
+            
+            // Check if the message is a server shutdown notice
+            if (strcmp(message, "Server is shutting down.\n") == 0) {
+                printf("%s - Server is shutting down. Exiting...\n", timestamp);
+                exit(EXIT_SUCCESS);  // Exit client program
+            }
+            
+            printf("%s - %s", timestamp, message);  // Print the timestamp and message
+            str_overwrite_stdout();  // Overwrite the stdout
+        } else if (receive == 0) {
+            printf("Server connection closed. Exiting...\n");
+            exit(EXIT_SUCCESS);  // Exit client program
+        } else {
+            perror("recv failed");  // Print the receive error
+            exit(EXIT_FAILURE);  // Exit client program due to error
+        }
     }
-  }
-  return NULL;
+    return NULL;
 }
 
 // Function to parse command-line arguments
 void parse_args(int argc, char *argv[], char *ip, int *port) {
-  if (argc < 2) { // Check if all required parameters are provided
+    if (argc < 2) { // Check if all required parameters are provided
         print_usage(argv[0]);
         exit(EXIT_FAILURE);
-  }
-  int opt;
-  // while ((opt = getopt(argc, argv, "u:r:p:a:")) != -1) {
-    while ((opt = getopt(argc, argv, "u:a:")) != -1) {
-    switch (opt) {
-    case 'u': // Username
-      strncpy(username, optarg, 31);
-      username[31] = '\0';
-      break;
-    // case 'r': // Real name
-    //   strncpy(realname, optarg, 31);
-    //   realname[31] = '\0';
-    //   break;
-    // case 'p': // Password
-    //   strncpy(password, optarg, 31);
-    //   password[31] = '\0';
-    //   break;
-    case 'a': // Address and port in format address:port
-      sscanf(optarg, "%14[^:]:%d", ip, port);
-      break;
-    default:
-      fprintf(stderr,
-              // "Usage: %s -u username -r realname -p password -a address:port\n",
-              // argv[0]);
-              "Usage: %s -u username -a address:port\n",
-              argv[0]);
-      exit(EXIT_FAILURE);
     }
-  }
+    int opt;
+    while ((opt = getopt(argc, argv, "u:a:")) != -1) {
+        switch (opt) {
+            case 'u': // Username
+                strncpy(username, optarg, 31);
+                username[31] = '\0';
+                break;
+            case 'a': // Address and port in format address:port
+                sscanf(optarg, "%14[^:]:%d", ip, port);
+                break;
+            default:
+                print_usage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
-  char ip[15] = "";
-  int port = -1;
+    char ip[15] = "";  // Array to store the server IP address
+    int port = -1;     // Variable to store the server port
 
   parse_args(argc, argv, ip, &port);
 
@@ -220,7 +206,6 @@ int main(int argc, char **argv) {
 
   // Send login details (username, real name, password) to the server
   char login_details[128];
-  // sprintf(login_details, "%s %s %s", username, realname, password);
   sprintf(login_details, "%s", username);
   send(sockfd, login_details, strlen(login_details), 0);
 
